29c23c6eedb9cfa263e12736a00c54c566d62c28
sudirboyy <sudirboyy@gmail.com>
2023-03-31 18:17:00 +0700

    Membuat inputan array

--- QuickSort_178/QuickSort_178.cpp
+++ QuickSort_178/QuickSort_178.cpp
@@ -6,3 +6,12 @@ int arr[20];                            // array of integers to hold values
 int cmp_count = 0;                      // number of comparasion 
 int mov_count = 0;                      // number of data movements
 int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+
+    }
