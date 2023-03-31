09d40a2b3017f170347b83122cf88fdde3dd6a63
sudirboyy <sudirboyy@gmail.com>
2023-03-31 18:14:37 +0700

    Membuat variabel array, perbandingan dan perpindahan array.

--- QuickSort_178/QuickSort_178.cpp
+++ QuickSort_178/QuickSort_178.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+// array of intergress to hold values
+int arr[20];                            // array of integers to hold values 
+int cmp_count = 0;                      // number of comparasion 
+int mov_count = 0;                      // number of data movements
+int n;
