ef9003efcd2fdc8dacc366e6e6c62f0a5be984f7
sudirboyy <sudirboyy@gmail.com>
2023-03-31 18:23:04 +0700

    Membuat fungsi

--- QuickSort_178/QuickSort_178.cpp
+++ QuickSort_178/QuickSort_178.cpp
@@ -103,3 +103,12 @@ void display() {
     cout << "\n\nNumber of comparasion: " << cmp_count << endl;
     cout << "Number of data movements: " << mov_count << endl;
 }
+int main()
+{
+    input();
+    q_short(0, n - 1);                                                      // Sort the array using quick sort 
+    display();
+    system("pause");
+
+    return 0;
+}
